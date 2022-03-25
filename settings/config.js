6b65c1df364aa659617b2c0d900df6e28c5357ed
xrrmli <80378609+xrrmli@users.noreply.github.com>
2022-03-25 19:11:07 +0700

    Update config.js

--- settings/config.js
+++ settings/config.js
@@ -7,11 +7,11 @@ global.anticall = false
 global.anti_delete = false
 
 // setting
-global.ownername ="ZeeoneOfc"
-global.ownernumber = "62887435047326"
-global.botname = "Haruka-Bot"
+global.ownername ="Ram"
+global.ownernumber = "6285892793169"
+global.botname = "R-Bot"
 global.thumbnail = fs.readFileSync("./settings/Haruka.jpg") //sesuaikan dengan nama foto
-global.background = "https://telegra.ph/file/d4c05638fa7886a1d8060.jpg"
+global.background = "https://telegra.ph/file/14a1fe50b978c37df3f89.jpg"
 global.lolkey = 'e966d3aac613b61dabd35cc6' //register lolhuman.xyz klo mau apikey
 global.zenzkey = '8423e19c13' //register zenzapi.xyz klo mau apikey
 global.limit = {
